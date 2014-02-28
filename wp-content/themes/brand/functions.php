386d5f87628ab77f36e029242502c3dfd33434bb
jeremyfelt <jeremy.felt@gmail.com>
2014-02-28 13:43:54 -0800

    Enqueue template Javasacript in footer of page
    This JS waits for document ready, so isn't required until the DOM is done loading anyway. Calling the script in the footer can help speed up the remainder of the page load.

--- wp-content/themes/brand/functions.php
+++ wp-content/themes/brand/functions.php
@@ -1,11 +1,7 @@
 <?php
 
 function brand_scripts() {
-	wp_enqueue_script(
-		'brand.js',
-		get_stylesheet_directory_uri() . '/scripts/brand.js',
-		array( 'jquery' )
-	);
+	wp_enqueue_script( 'brand.js', get_stylesheet_directory_uri() . '/scripts/brand.js', array( 'jquery' ), false, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'brand_scripts' );
