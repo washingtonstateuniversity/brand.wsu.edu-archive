79c3994caf29718c1f80c3b95bacd8320f02424b
jeremyfelt <jeremy.felt@gmail.com>
2014-02-28 16:28:12 -0800

    Replace esc_html() with wp_kses_post() where output will contain HTML tags

--- wp-content/themes/brand/page-tone.php
+++ wp-content/themes/brand/page-tone.php
@@ -95,7 +95,7 @@ ol li {
 	<article>
 		<?php 
 		$column = get_post_meta( get_the_ID(), 'section-2-1', true );
-		if( ! empty( $column ) ) { echo esc_html( $column ); }
+		if( ! empty( $column ) ) { echo wp_kses_post( $column ); }
 		?>
 	</article>	
 	</div>
@@ -110,7 +110,7 @@ ol li {
 	<article>
 		<?php 
 		$column = get_post_meta( get_the_ID(), 'section-3-1', true );
-		if( ! empty( $column ) ) { echo esc_html( $column ); }
+		if( ! empty( $column ) ) { echo wp_kses_post( $column ); }
 		?>
 	</article>
 	</div>
