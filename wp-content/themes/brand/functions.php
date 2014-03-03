ce6f9328ea6c9b513add75867e4d61c9e38d3518
Nate Jacobson <nate.jacobson@wsu.edu>
2014-03-03 11:05:41 -0800

    Add script for sticky header

--- wp-content/themes/brand/functions.php
+++ wp-content/themes/brand/functions.php
@@ -5,6 +5,7 @@ add_action( 'wp_enqueue_scripts', 'brand_scripts' );
  * Enqueue child theme Javascript files.
  */
 function brand_scripts() {
+	wp_enqueue_script( 'jquery.hc-sticky.min.js', get_stylesheet_directory_uri() . '/scripts/jquery.hc-sticky.min.js', array( 'jquery' ), false, true );
 	wp_enqueue_script( 'brand.js', get_stylesheet_directory_uri() . '/scripts/brand.js', array( 'jquery' ), false, true );
 }
 
