ce6b9c26203047d4f5753eca068817456e3b26bb
Nate Jacobson <nate.jacobson@wsu.edu>
2014-04-09 09:31:08 -0700

    Make sure sticky header works with new class names

--- wp-content/themes/brand/scripts/brand.js
+++ wp-content/themes/brand/scripts/brand.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
 	
 	$('body[class*="-90"] dl dt').click( function () { $(this).next('dd').toggle().parents('dl').toggleClass('disclosed'); })
 	
-	$('.size-gt-medium .brand-header').hcSticky({
+	$('.size-gt-medium .main-header, .size-gt-medium .brand-header').hcSticky({
 	    top: 0,
 	    wrapperClassName: 'affixed'
 	});
