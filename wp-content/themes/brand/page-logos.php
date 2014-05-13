ab194c9463e3126c6f7fd3f97776e4092334a773
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-05-13 09:34:31 -0700

    Start "Do Not" x background at top instead

--- wp-content/themes/brand/page-logos.php
+++ wp-content/themes/brand/page-logos.php
@@ -81,7 +81,7 @@ button.detail header:after {
 	width: 792px;
 	}
 .size-lt-large .donot {
-	background: url('/wp-content/themes/brand/images/elements/x-not-50.png') center center repeat-y;
+	background: url('/wp-content/themes/brand/images/elements/x-not-50.png') center top repeat-y;
 	background-size: 550px auto;
 	}
 
